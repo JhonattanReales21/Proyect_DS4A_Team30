9861d9c7e2cb2d72975a06c9e583c50d14ed4dd8
hmejia <hmejia@payvalida.com>
2020-11-06 19:20:55 -0500

    Creates instant datafetcher for cosine matrix

--- data_fetch/get_views.py
+++ data_fetch/get_views.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from os import environ
-
+from data_fetch.unpivoting import sub_pivots
 
 DB_URI = environ['URI']
 ENGINE = create_engine(DB_URI)
@@ -18,3 +18,20 @@ def get_view_by_name(view_name):
         select * from {0}
     """.format(view_name)
     return pd.read_sql_query(query, con=ENGINE)
+
+
+def get_cosine_views(city):
+    """
+    This function is intended to fetch the cosine_similarity of article types in a city 
+    in a matrix representation.
+    params:
+        - city: city to filter
+    return:
+        :pd.Dataframe containing the cosine similarity as a Matrix Representation
+    """
+    query = """
+        select * from cosine_similarity where city = '{0}'
+    """.format(city)
+    data = pd.read_sql_query(query, con=ENGINE)
+
+    return sub_pivots(data, 'tipo_articulo_1', 'tipo_articulo_2', 'cosine_similarity')
