e21ba6f42b763a8bf6b49beac9a7ae9b0529f1bb
santiagobs <santiagoa.bohorquez@gmail.com>
2020-11-07 20:44:09 -0500

    Updated general style of the app

--- scripts/tabs.py
+++ scripts/tabs.py
@@ -3,15 +3,30 @@ import dash_html_components as html
 
 from scripts import dashboard, analysis, segmentation
 
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'color':'#0e0817',
+    'font-weight': 'bold'
+}
+
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#07040d',
+    'color': 'white',
+    'font-weight': 'bold'
+}
+
 def create_tab():
 
     return dcc.Tabs(
         id="tabs",
+        className="custom-tab",
         value='Dashboard',
         children=[
-            dcc.Tab(label='Dashboard', value='Dashboard'),
-            dcc.Tab(label='Analysis', value='Analysis'),
-            dcc.Tab(label='Segmentation', value='Segmentation')
+            dcc.Tab(label='Dashboard', value='Dashboard', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='Analysis', value='Analysis', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='Segmentation', value='Segmentation', style=tab_style, selected_style=tab_selected_style)
         ],
         colors={
             "border": "white",
