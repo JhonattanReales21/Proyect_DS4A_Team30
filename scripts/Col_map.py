e42c50695514c75c3b2195459033e938f9f3948d
Jhonattan Reales <jhonatanreales21@gmail.com>
2020-11-07 20:17:39 -0500

    Fix bug

--- scripts/Col_map.py
+++ scripts/Col_map.py
@@ -118,7 +118,7 @@ def create_map():
 	# Map Layout
 	##############################
 	map = html.Div(
-	    [   
+	    [
 			html.H1("Sales frequency and amount per store"),
 	        # Place the main graph component here:
 	        html.Iframe(srcDoc = open('Colombia_map.html','r').read()
