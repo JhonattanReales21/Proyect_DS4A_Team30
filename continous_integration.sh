ff01b67959dacf98d3e26e40401cb7a703273b7c
hmejia <hmejia@payvalida.com>
2020-11-13 22:52:48 -0500

    refactor ci

--- continous_integration.sh
+++ continous_integration.sh
@@ -1,5 +1,5 @@
 sudo git pull
 sudo docker rm $(docker ps -qa) -f
-sudo docker build -t dash . 
+sudo docker build -t dash:latest . 
 sudo docker run -it -d -p 8050:8050  -v "$(PWD)":/app --rm dash:latest /bin/bash -c 'BUCKET_NAME=ds4a-team-30 URI=postgres://postgres:postgres@database-team30.cn9ikgjikjdg.us-east-2.rds.amazonaws.com:5432/postgres python3 index.py'
 sudo docker system prune -af
