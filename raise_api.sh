210c6b9b15a08aed9d4cdf315af7ca6962f64947
hmejia <hmejia@payvalida.com>
2020-11-14 21:32:07 -0500

    Instalation of Python3.7 in API Server

--- raise_api.sh
+++ raise_api.sh
@@ -1,4 +1,8 @@
-sudo yum update -y
-sudo yum install -y python3
-sudo yum install -y python3-pip
-pip install -r requirements.txt
+sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel zlib-devel
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+sudo tar xzf Python-3.7.9.tgz
+cd Python-3.7.9
+sudo ./configure --enable-optimizations
+sudo make altinstall
+sudo rm /usr/src/Python-3.7.9.tgz
